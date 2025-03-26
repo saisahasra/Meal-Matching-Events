538e41d13892c8ca5c21b80e8bc8f228938052ff
HANA ALMOGBEL <H.ALMOGBEL-2022@hull.ac.uk>
2025-03-26 03:55:24 +0000

    Implemented login page with basic form and authentication against users.json. Added a feed component to display user profiles with name, age, course, hobbies, and profile picture.

---
+++ Connectify/Connectify/Components/Pages/_Host.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Connectify.Components.Pages
+{
+    public class _HostModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Connectify/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ConnectifyApp.Models
+{
+    public class User
+    {
+        [JsonPropertyName("UserID")]
+        public int UserID { get; set; }
+
+        [JsonPropertyName("Username")]
+        public string Username { get; set; }
+
+        [JsonPropertyName("Password")]
+        public string Password { get; set; }
+
+        [JsonPropertyName("FirstName")]
+        public string FirstName { get; set; }
+
+        [JsonPropertyName("Age")]
+        public int Age { get; set; }
+
+        [JsonPropertyName("Course")]
+        public string Course { get; set; }
+
+        [JsonPropertyName("Hobbies")]
+        public string Hobbies { get; set; }
+
+        [JsonPropertyName("AboutMe")]
+        public string AboutMe { get; set; }
+
+        [JsonPropertyName("ProfilePicture")]
+        public string ProfilePicture { get; set; }
+    }
+}
